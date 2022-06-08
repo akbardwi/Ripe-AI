64e55e8f0511d3b1dd723711ea4f1b08f9d0709f
Rayhan Haqi <tilarayhan@gmail.com>
2022-06-08 20:52:08 +0700

    ML bisa deploy dan request image prediction

--- Machine Learning/Script/Inference.py
+++ Machine Learning/Script/Inference.py
@@ -1,4 +1,5 @@
 from urllib import response
+from matplotlib import image
 import requests
 import json
 import numpy as np
@@ -6,14 +7,17 @@ import cv2 as cv
 import tensorflow as tf
 import time
 
-img = cv.imread('image.jpg')
+img = cv.imread('/home/rayhan/Ripe-AI/Machine Learning/Script/image.jpeg')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 img = cv.resize(img, (150,150))
+img = np.array(img)
 img = np.expand_dims(img, axis=0)
+img = img / 255
 
-url = ''
-data = json.dumps({"signature_name": "", "instances": })
+
+url = 'http://localhost:8501/v1/models/Banana:predict'
+data = json.dumps({"signature_name": "", "instances": img.tolist()})
 headers = {"content-type": "application/json"}
 response = requests.post(url, data = data)
-prediction = json.loads(response.text)["prediction"]
+prediction = json.loads(response.text)["predictions"]
 print(prediction)

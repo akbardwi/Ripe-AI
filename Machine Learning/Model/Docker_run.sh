bc14ed0f1bd6c9e9851f6054f7bf250761bc8cac
Rayhan Haqi <tilarayhan@gmail.com>
2022-06-08 22:26:41 +0700

    Nambah file bash docker run

--- Machine Learning/Model/Docker_run.sh
+++ Machine Learning/Model/Docker_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker run -p 8501:8501 -p 8500:8500 --mount type=bind,source=/home/rayhan/Ripe-AI/Machine\ Learning/Model/Banana,target=/models/Banana -e MODEL_NAME=Banana -t tensorflow/serving:latest
+sudo docker run -p 8501:8501 -p 8500:8500 --mount type=bind,source=$HOME/Ripe-AI/Machine\ Learning/Model/Banana,target=/models/Banana -e MODEL_NAME=Banana -t tensorflow/serving:latest
+
+sudo docker run -p 8501:8501 -p 8500:8500 --mount type=bind,source=$PWD/Banana,target=/models/Banana -e MODEL_NAME=Banana -t tensorflow/serving:latest

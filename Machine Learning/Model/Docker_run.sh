cba73b23f1435e72064b6fd241d46f0037b19a3f
Rayhan Haqi <tilarayhan@gmail.com>
2022-06-09 11:13:21 +0700

    Debug

--- Machine Learning/Model/Docker_run.sh
+++ Machine Learning/Model/Docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --name Banana -p 8501:8501 -p 8500:8500 --mount type=bind,source=$PWD/Ripe-AI/Machine\ Learning/Model/Banana,target=/models/Banana -e MODEL_NAME=Banana -t tensorflow/serving:latest
+sudo docker run --name Banana -p 8501:8501 -p 8500:8500 --mount type=bind,source=$HOME/Ripe-AI/Machine\ Learning/Model/Banana,target=/models/Banana -e MODEL_NAME=Banana -t tensorflow/serving:latest

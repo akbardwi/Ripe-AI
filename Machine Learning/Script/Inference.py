f90d91522ee9b3d6bb63555690abbab69cb74bee
Rayhan Haqi <tilarayhan@gmail.com>
2022-06-09 09:57:21 +0700

    Nambah keterangan index di response prediction

--- Machine Learning/Script/Inference.py
+++ Machine Learning/Script/Inference.py
@@ -7,7 +7,7 @@ import cv2 as cv
 import tensorflow as tf
 import time
 
-img = cv.imread('/home/rayhan/Ripe-AI/Machine Learning/Script/image.jpeg')
+img = cv.imread('/home/rayhan/Ripe-AI/Machine Learning/Script/Ripe 1.jpeg')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 img = cv.resize(img, (150,150))
 img = np.array(img)
@@ -21,3 +21,9 @@ headers = {"content-type": "application/json"}
 response = requests.post(url, data = data)
 prediction = json.loads(response.text)["predictions"]
 print(prediction)
+
+
+#    Saat print prediction:
+#       Index 0 = Overripe
+#       Index 1 = Ripe 
+#       Index 2 = Unripe 

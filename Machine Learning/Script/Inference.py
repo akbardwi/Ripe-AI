b558df060e8625a745c22b15bc9ed1deffe29c0c
Rayhan Haqi <tilarayhan@gmail.com>
2022-06-11 10:46:52 +0700

    Edit Inference.py

--- Machine Learning/Script/Inference.py
+++ Machine Learning/Script/Inference.py
@@ -15,12 +15,18 @@ img = np.expand_dims(img, axis=0)
 img = img / 255
 
 
-url = 'http://localhost:8501/v1/models/Banana:predict'
+url = 'http://34.70.38.194:8501/v1/models/Banana:predict'
 data = json.dumps({"signature_name": "", "instances": img.tolist()})
 headers = {"content-type": "application/json"}
 response = requests.post(url, data = data)
 prediction = json.loads(response.text)["predictions"]
-print(prediction)
+# print(prediction)
+
+predict_dict = {"Overripe": prediction[0][0], "Ripe": prediction[0][1], "Unripe": prediction[0][2]}
+predict_dict = sorted(predict_dict.items(), key=lambda x: x[1], reverse=True)
+
+for i, j in predict_dict:
+    print("{:>8} = {:>13.10f} %" .format(i, j*100))
 
 
 #    Saat print prediction:
